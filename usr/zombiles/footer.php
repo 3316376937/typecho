55f3f3467e0b89a5954d6743363af59c21dd8a75
3316376937 <139975242+3316376937@users.noreply.github.com>
2024-03-24 21:44:59 +0800

    1

--- usr/zombiles/footer.php
+++
@@ -1,8 +0,0 @@
-</div>
-	<div class="grid_14" id="footer">
-	<a href="<?php $this->options->siteurl(); ?>"><?php $this->options->title(); ?></a> <?php _e('is powered by'); ?> <a href="http://www.typecho.org">Typecho)))</a> | Theme Created By <a href="http://blog.imnerd.org" target="_blank">公子</a> & Designed By BlogBus<br /><a href="<?php $this->options->feedUrl(); ?>"><?php _e('文章'); ?> RSS</a> | <a href="<?php $this->options->commentsFeedUrl(); ?>"><?php _e('评论'); ?> RSS</a> | <a class="top" href="#" title="返回顶部">返回顶部</a>
-	</div><!-- end #footer -->
-<script type="text/javascript" src="<?php $this->options->themeUrl('js/function.js'); ?>">"></script>
-<?php $this->footer(); ?>
-</body>
-</html>
