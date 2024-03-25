aa5e3897966d021c3e520c502eb0e42fbd0d642a
3316376937 <139975242+3316376937@users.noreply.github.com>
2024-03-25 12:20:51 +0800

    1

--- config.inc.php
+++ config.inc.php
@@ -41,7 +41,7 @@ $db->addServer(array (
   'password' => 'Z2Krox3CNW4u5h0Q81fmciAXn769vVPS',
   'charset' => 'utf8mb4',
   'port' => '31238',
-  'database' => 'railway',
+  'database' => 'zeabur',
   'engine' => 'MyISAM',
 ), Typecho_Db::READ | Typecho_Db::WRITE);
 Typecho_Db::set($db);
